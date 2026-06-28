8d9ad4e84b0a632fde638a92f5c6baee42cf18bf
YeCha-cpu <3087565445@qq.com>
2026-06-28 17:00:10 +0800

    实现了在背包界面中右键槽位时会显示 功能UI 的功能，并修复了一次性拾取多个相同物品时的背包槽位的计数bug

---
+++ Source/DoomsdaySurvival/Public/Core/G_GameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "G_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DOOMSDAYSURVIVAL_API UG_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
